c83adbe178d0c58319bf065f8f04858ada6f8562
Zhao Zhenlong <zhaozhl@inspur.com>
2015-12-10 00:12:13 +0800

    add json_write to BaseHandler

--- ics_demo/handlers/base.py
+++ ics_demo/handlers/base.py
@@ -1,5 +1,9 @@
+import json
 import tornado.web
 
 class BaseHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("ICS Demo")
+
+    def json_write(self, obj):
+        self.write(json.dumps(obj.__dict__, default=lambda o: o.__dict__))
