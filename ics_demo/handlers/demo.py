66792c3aa2d0c6bbbd3a969db278f1cce7743e03
Zhao Zhenlong <zhaozhl@inspur.com>
2015-12-12 03:08:26 +0800

    add get instructions

--- ics_demo/handlers/demo.py
+++ ics_demo/handlers/demo.py
@@ -12,6 +12,14 @@ class DemoBaseHandler(BaseHandler):
 
 class DemoRabbitHandler(DemoBaseHandler):
     def get(self, rabbit_id=None):
+        """
+            Instructions:
+            In GET:
+                1. fetch data from dao,
+                    BaseHandler provide method get_from_dao to do some generic operation
+                2. display data and convert it to json
+                    using json.dumps enough and fill type
+        """
         self.write(json.dumps({'rabbit': self.get_from_dao(RabbitDAO, rabbit_id)}))
 
 class DemoCarrotHandler(DemoBaseHandler):
