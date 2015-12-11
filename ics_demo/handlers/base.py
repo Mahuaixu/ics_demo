deadbb23f3ff6fa434862605c4a8609a696385ec
Zhao Zhenlong <zhaozhl@inspur.com>
2015-12-11 22:18:58 +0800

    add get_all_or_one to BaseHandler

--- ics_demo/handlers/base.py
+++ ics_demo/handlers/base.py
@@ -1,4 +1,5 @@
 import tornado.web
+from sqlobject import *
 
 from ics_demo.helpers.convert import sqlist2list, sqlobj2dict
 
@@ -13,3 +14,12 @@ class BaseHandler(tornado.web.RequestHandler):
     def get_one(self, cls, identifier):
         obj = cls.get(identifier)
         return sqlobj2dict(obj)
+
+    def get_all_or_one(self, cls, identifier):
+        try:
+            if not identifier:
+                return self.get_all(cls)
+            else:
+                return self.get_one(cls, identifier)
+        except SQLObjectNotFound:
+            raise tornado.web.HTTPError(404)
--- ics_demo/handlers/demo.py
+++ ics_demo/handlers/demo.py
@@ -1,7 +1,6 @@
 import json
 
 import tornado
-from sqlobject import *
 
 from ics_demo.handlers import BaseHandler
 from ics_demo.dao.orm.demo import Rabbit
@@ -12,11 +11,5 @@ class DemoBaseHandler(BaseHandler):
 
 class DemoRabbitHandler(DemoBaseHandler):
     def get(self, rabbit_id=None):
-        try:
-            if not rabbit_id:
-                rabbit = self.get_all(Rabbit)
-            else:
-                rabbit = self.get_one(Rabbit, rabbit_id)
-        except SQLObjectNotFound:
-            raise tornado.web.HTTPError(404)
+        rabbit = self.get_all_or_one(Rabbit, rabbit_id)
         self.write(json.dumps({'rabbit': rabbit}))
