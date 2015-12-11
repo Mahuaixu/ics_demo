3a0062a4520f49d1a4297c5e7af3fe0f987a0d41
Zhao Zhenlong <zhaozhl@inspur.com>
2015-12-11 23:15:40 +0800

    Add exceptions in exc.py

---
+++ ics_demo/helpers/exc.py
@@ -0,0 +1,23 @@
+class IcsError(Exception):
+    """
+    Unknown ics error
+    """
+
+    def __str__(self):
+        doc = self.__doc__.strip()
+        return ': '.join([doc] + [str(a) for a in self.args])
+
+class NotFoundError(IcsError):
+    """
+    No data found
+    """
+    def handle(self):
+        raise tornado.web.HTTPError(404, self.__doc__.strip())
+
+class CannotParsedError(IcsError):
+    """
+    Data cannot be parsed
+    """
+    def handle(self):
+        raise tornado.web.HTTPError(500, self.__doc__.strip())
+
