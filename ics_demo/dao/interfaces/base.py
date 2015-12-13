cb67329451db7470c5a13759dedb0d1509cbdcbd
Zhao Zhenlong <zhaozhl@inspur.com>
2015-12-14 02:55:06 +0800

    add blacklist to dismiss id in SQLObject

--- ics_demo/dao/interfaces/base.py
+++ ics_demo/dao/interfaces/base.py
@@ -6,14 +6,14 @@ from ics_demo.helpers.convert import sqlist2list, sqlobj2dict
 class BaseDAO(object):
     def _get_all_from_orm(self, klass):
         objs = klass.select()
-        return sqlist2list(objs)
+        return sqlist2list(objs, blacklist=['id'])
 
     def _get_one_from_orm(self, klass, identifier):
         try:
             obj = klass.selectBy(uuid=identifier).getOne()
         except SQLObjectNotFound:
             raise NotFoundError()
-        return sqlobj2dict(obj)
+        return sqlobj2dict(obj, blacklist=['id'])
 
     def _get_one_from_livestatus(self, klass, identifier):
         pass
--- ics_demo/helpers/convert.py
+++ ics_demo/helpers/convert.py
@@ -2,18 +2,24 @@ import json, jsonpickle
 
 from exc import CannotParsedError
 
-def sqlobj2dict(sqlobj):
+def sqlobj2dict(sqlobj, blacklist=None):
     #FIXME: dealing Datetime()
-    return sqlobj.sqlmeta.asDict()
+    blacklist = list(blacklist or [])
+    # result = {}
+    # for key,val in sqlobj.sqlmeta.asDict().iteritems():
+    #     if key not in blacklist:
+    #         result[key] = val
+    # return result
+    return dict((key, val) for key, val in sqlobj.sqlmeta.asDict().iteritems() if key not in blacklist)
 
-def sqlobj2json(sqlobj):
-    return jsonpickle.encode(sqlobj2dict(sqlobj), unpicklable=True)
+def sqlobj2json(sqlobj, blacklist=None):
+    return jsonpickle.encode(sqlobj2dict(sqlobj, blacklist), unpicklable=True)
 
-def sqlist2list(sqlist):
-    return [sqlobj2dict(sqlobj) for sqlobj in sqlist]
+def sqlist2list(sqlist, blacklist=None):
+    return [sqlobj2dict(sqlobj, blacklist) for sqlobj in sqlist]
 
-def sqlist2json(sqlist):
-    return jsonpickle.encode(sqlist2list(sqlist), unpicklable=True)
+def sqlist2json(sqlist, blacklist=None):
+    return jsonpickle.encode(sqlist2list(sqlist, blacklist), unpicklable=True)
 
 def json2dict(json_str):
     try:
