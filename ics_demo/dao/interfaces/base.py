388039cfdb1470cfd6d96e55ac8202a20b656d7b
Zhao Zhenlong <zhaozhl@inspur.com>
2015-12-14 06:45:14 +0800

    add id_to_ref to convert SQLObject ID:id to Ref:uuid

--- ics_demo/dao/interfaces/base.py
+++ ics_demo/dao/interfaces/base.py
@@ -2,6 +2,20 @@ from sqlobject import *
 
 from ics_demo.helpers.exc import NotFoundError
 from ics_demo.helpers.convert import sqlist2list, sqlobj2dict
+from ics_demo.dao.interfaces.mapping import ref_mapping
+
+# convert xxxID:id to xxxRef:identifier
+def id_to_ref(output_dict):
+    result = {}
+    for key, val in output_dict.iteritems():
+        if key.endswith("ID"):
+            ref_key = key[:-2]
+            klass = ref_mapping.get(ref_key, None)
+            if ref_key in ref_mapping.keys():
+               result[ref_key + 'Ref'] = klass.get(val).get_identifier()
+        else:
+            result[key] = val
+    return result
 
 class BaseDAO(object):
     def _get_all_from_orm(self, klass):
@@ -16,10 +30,10 @@ class BaseDAO(object):
         return sqlobj2dict(obj, blacklist=['id'])
 
     def get_all_by_class(self, klass):
-        return self._get_all_from_orm(klass)
+        return map(id_to_ref, self._get_all_from_orm(klass))
 
     def get_one_by_class(self, klass, identifier):
-        return self._get_one_from_orm(klass, identifier)
+        return id_to_ref(self._get_one_from_orm(klass, identifier))
 
     def get_keys_by_class(self, klass):
         return klass.sqlmeta.getColumns().keys()
---
+++ ics_demo/dao/interfaces/mapping.py
@@ -0,0 +1,4 @@
+from ics_demo.dao.orm.demo import Rabbit, Carrot, Corps
+
+demo_ref_mapping = {'rabbit' : Rabbit, 'carrot' : Carrot, 'corps' : Corps}
+ref_mapping = dict(demo_ref_mapping.items())
