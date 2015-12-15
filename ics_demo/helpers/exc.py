dcfa1fdcea8d272c185a16025615c7c74b222b86
Zhao Zhenlong <zhaozhl@inspur.com>
2015-12-15 15:00:43 +0800

    rewrite IcsError to show message at backend

--- ics_demo/helpers/exc.py
+++ ics_demo/helpers/exc.py
@@ -4,49 +4,48 @@ class IcsError(tornado.web.HTTPError):
     """
     Unknown ics error
     """
-    def __init__(self, status_code, message):
-        super(tornado.web.HTTPError, self).__init__(status_code, log_message=message, reason=message)
+    def __init__(self, status_code):
+        super(tornado.web.HTTPError, self).__init__(status_code, log_message=self.message, reason=self.message)
 
     def __str__(self):
-        doc = self.__doc__.strip()
-        return ': '.join([doc] + [str(a) for a in self.args])
+        return self.message
 
 class NotFoundError(IcsError):
     """
     No data found
     """
-    def __init__(self, what, who):
-        message = self.__doc__.strip() + ': ['+ str(what) + '] of ' + str(who)
-        super(IcsError, self).__init__(404, message)
+    def __init__(self, which_type, which_item):
+        self.message = self.__doc__.strip() + ': ['+ str(which_type) + '] of ' + str(which_item)
+        super(IcsError, self).__init__(404)
 
 class CannotParsedError(IcsError):
     """
     Data cannot be parsed
     """
     def __init__(self, to_what, from_what):
-        message = self.__doc__.strip() + ' to ['+ str(to_what) + '] of ' + str(from_what)
-        super(IcsError, self).__init__(500, message)
+        self.message = self.__doc__.strip() + ' to ['+ str(to_what) + '] of ' + str(from_what)
+        super(IcsError, self).__init__(500)
 
 class DuplicatedError(IcsError):
     """
     Data duplicated
     """
     def __init__(self, dup_type, data):
-        message = self.__doc__.strip() + ' type ['+ str(dup_type) + '] duplicated: ' + str(data)
-        super(IcsError, self).__init__(500, message)
+        self.message = self.__doc__.strip() + ' type ['+ str(dup_type) + '] duplicated: ' + str(data)
+        super(IcsError, self).__init__(500)
 
 class FailedExecError(IcsError):
     """
     Failed Executed
     """
     def __init__(self, cmd, reason):
-        message = self.__doc__.strip() + ' ['+ str(cmd) + '] reason: ' + str(reason)
-        super(IcsError, self).__init__(500, message)
+        self.message = self.__doc__.strip() + ' ['+ str(cmd) + '] reason: ' + str(reason)
+        super(IcsError, self).__init__(500)
 
 class AlreadyExistsError(IcsError):
     """
     Already Exists
     """
     def __init__(self, what_exists, where):
-        message = self.__doc__.strip() + ' ['+ str(what_exists) + '] on: ' + str(where)
-        super(IcsError, self).__init__(500, message)
+        self.message = self.__doc__.strip() + ' ['+ str(what_exists) + '] on: ' + str(where)
+        super(IcsError, self).__init__(500)
