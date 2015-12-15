c12ef7242ff6ab38026573c2531fa4614786490f
Zhao Zhenlong <zhaozhl@inspur.com>
2015-12-15 15:15:00 +0800

    init_proxy when web service starts

--- ics_demo/main.py
+++ ics_demo/main.py
@@ -1,11 +1,13 @@
 import tornado.ioloop
 import tornado.web
 from ics_demo.dao import init_db
+from ics_demo.remote_services import init_proxy
 from ics_demo.routes import urls
 
 application = tornado.web.Application(urls)
 
 if __name__ == "__main__":
     init_db()
+    init_proxy()
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
