e4825246238882f7f75a8eb5cd1d8ac8fd17efd0
Zhao Zhenlong <zhaozhl@inspur.com>
2015-12-15 15:11:19 +0800

    add remote_cmd_quiet method to Service

--- ics_demo/remote_services/base.py
+++ ics_demo/remote_services/base.py
@@ -1,4 +1,4 @@
-from ics_demo.helpers.remotes import remote_cmd_list, remote_cmd_oneline, remote_cmd_json
+from ics_demo.helpers.remotes import remote_cmd_list, remote_cmd_oneline, remote_cmd_json, remote_cmd_quiet
 
 class Service(object):
     def __init__(self, proxy):
@@ -12,3 +12,6 @@ class Service(object):
 
     def remote_cmd_json(self, cmd_str):
         return remote_cmd_json(self.conn, cmd_str)
+
+    def remote_cmd_quiet(self, cmd_str):
+        return remote_cmd_quiet(self.conn, cmd_str)
