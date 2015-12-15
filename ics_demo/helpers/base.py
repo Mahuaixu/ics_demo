aaa2395969a2809f796334e2c74f6c6f7969cfe0
Zhao Zhenlong <zhaozhl@inspur.com>
2015-12-15 09:30:03 +0800

    add check ip addr helper

--- ics_demo/helpers/base.py
+++ ics_demo/helpers/base.py
@@ -1,4 +1,11 @@
 import uuid
+import socket
 
 def uuidgen():
     return str(uuid.uuid4())
+
+def valid_ipaddr(ipaddr):
+    try:
+        socket.inet_aton(ipaddr)
+    except:
+        raise CannotParsedError('ip address', ipaddr)
